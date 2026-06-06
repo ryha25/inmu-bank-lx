4654cb6f0219bb88bc8c0b6e95ce1adfa03e1fef
ryha25 <ryuyah12@gmail.com>
2026-06-07 01:53:54 +0900

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,5 @@
 import { AuthForm } from '@/components/auth-form'
-import { getSession } from '@/lib/auth'
-import { redirect } from 'next/navigation'
 
-export default async function SignInPage() {
-  const session = await getSession()
-  if (session?.user) redirect('/')
+export default function SignInPage() {
   return <AuthForm mode="sign-in" />
 }
